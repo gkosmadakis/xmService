49d480236d2e6abad46947f0c63952b982cb5c77
George <gekosmadakis@gmail.com>
2025-01-29 09:57:18 +0200

    initital commit

---
+++ src/test/java/com/xm/recommendationservice/RecommendationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xm.recommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
